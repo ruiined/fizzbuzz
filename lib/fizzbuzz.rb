801331d038483e6b68b1aec017266318e075ed57
Maria <ruiined@gmail.com>
2022-02-15 14:34:44 +0000

    added two more tests on fizzbuzz spec and finished the requirements on fizzbuzz file

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,11 @@
 def fizzbuzz(n)
-  if n % 3 == 0
+  if n % 3 == 0 && n % 5 == 0
+    "fizzbuzz"
+  elsif n % 3 == 0
     "fizz"
   elsif n % 5 == 0
     "buzz"
+  else
+    n
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,10 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do 
     expect(fizzbuzz(5)).to eq 'buzz'
   end  
+  it 'returns "fizzbuzz" when passed 3 and 5' do 
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns 7 when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
 end
